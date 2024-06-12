bf8ddd88d5040fbeb6fd3d64be5abfc72924498d
Lefteris <lefteriskoutrakis@yahoo.com>
2024-06-12 11:07:03 +0300

    Convert Exception to IOException..

--- Identity/src/main/java/gr/indice/identity/utils/Exceptions.kt
+++ Identity/src/main/java/gr/indice/identity/utils/Exceptions.kt
@@ -1,10 +1,11 @@
 package gr.indice.identity.utils
 
 import okhttp3.ResponseBody
+import java.io.IOException
 
 /**
  * Throw when something goes wrong with Identity.
  * @param code [Int]
  * @param error [ResponseBody]
  */
-class ServiceErrorException(val code: Int?, val error: ResponseBody): Exception()
+class ServiceErrorException(val code: Int?, val error: ResponseBody): IOException()
